606fb7f82b71985866df390919132c45a63ae709
Tonthimytho <nguyenthienanh156@gmail.com>
2023-03-17 22:27:49 +0700

    hoan thanh bai tap tuan 2

--- BIGSCHOOL/Controllers/CoursesController.cs
+++ BIGSCHOOL/Controllers/CoursesController.cs
@@ -1,5 +1,6 @@
 ﻿using BIGSCHOOL.Models;
 using BIGSCHOOL.ViewModels;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,8 @@ namespace BIGSCHOOL.Controllers
         {
             _dbContext = new ApplicationDbContext();
         }
+        
+        [HttpGet]
         public ActionResult Create()
         {
             var viewModel = new CourseViewModel
@@ -23,6 +26,27 @@ namespace BIGSCHOOL.Controllers
                 Categories = _dbContext.Categories.ToList()
             };
             return View(viewModel);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create",viewModel);
+            }    
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
